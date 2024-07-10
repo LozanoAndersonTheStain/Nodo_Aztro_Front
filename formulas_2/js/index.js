5a77cb546d3d07b0e37358c0d1fffe2ed872fad3
Anderson Lozano <adslozanov@gmail.com>
2024-07-09 23:58:57 -0500

    The first base of the form and the first page are made and the js of the index cards are modified.

--- formulas_2/js/index.js
+++ formulas_2/js/index.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const formulas = [
-        "Sacar el promedio de distancia de un nadador", "Calcular cuanto pesticida debe usarse",
-        "Calcular cuantos ladrillos deben usarse para hacer una pared", "Calcular cuanta masa corporal tiene una persona",
-        "Calcular la liquidación para tus empleados"
-    ]
+        { title: "Sacar el promedio de distancia de un nadador", url: "../../Pages/Ecuaciones/promedio_temporal.html" },
+        { title: "Calcular cuanto pesticida debe usarse", url: "#" },
+        { title: "Calcular cuantos ladrillos deben usarse para hacer una pared", url: "#" },
+        { title: "Calcular cuanta masa corporal tiene una persona", url: "#" },
+        { title: "Calcular la liquidación para tus empleados", url: "#" }
+    ];
     const container = document.querySelector(".container__cards");
 
-    formulas.forEach(formmula => {
-        const card = document.createElement("div");
+    formulas.forEach(formula => {
+        const card = document.createElement("a");
         card.className = "card";
-        card.textContent = formmula;
+        card.textContent = formula.title;
+        card.href = formula.url;
 
-        container.appendChild(card)
-    })
-})
+        container.appendChild(card);
+    });
+});
