286cb4dd1c2dc299498bdb68fc9c3c3e9e124990
Anderson Lozano <adslozanov@gmail.com>
2024-07-17 19:38:26 -0500

    Fixed bug, now you can use Float type values

--- formulas_2/js/ladrillos.js
+++ formulas_2/js/ladrillos.js
@@ -1,10 +1,10 @@
 document.getElementById("btnCalcular").addEventListener("click", function () {
-    const largoLadrillo = parseFloat(document.getElementById("param1").value);
-    const altoLadrillo = parseFloat(document.getElementById("param2").value);
-    const alturaPared = parseFloat(document.getElementById("param3").value) * 100; // Convertir a centímetros
-    const anchoPared = parseFloat(document.getElementById("param4").value) * 100; // Convertir a centímetros
-    const grosorVertical = parseFloat(document.getElementById("param5").value) || 0; // Valor por defecto 0 si no se ingresa
-    const grosorLateral = parseFloat(document.getElementById("param6").value) || 0; // Valor por defecto 0 si no se ingresa
+    const largoLadrillo = parseFloat(document.getElementById("param1").value) * 100;
+    const altoLadrillo = parseFloat(document.getElementById("param2").value) * 100;
+    const alturaPared = parseFloat(document.getElementById("param3").value) * 100;
+    const anchoPared = parseFloat(document.getElementById("param4").value) * 100;
+    const grosorVertical = parseFloat(document.getElementById("param5").value) || 0;
+    const grosorLateral = parseFloat(document.getElementById("param6").value) || 0;
 
     if (isNaN(largoLadrillo) || isNaN(altoLadrillo) || isNaN(alturaPared) || isNaN(anchoPared)) {
         alert("Por favor, ingrese valores numéricos en todos los campos");
